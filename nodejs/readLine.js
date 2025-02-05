fddfe424db31cb2f0252f500ffba2b40c4f28937
dena <devdenn7@gmail.com>
2025-02-05 09:22:49 +0700

    fix: refactor code

--- nodejs/readLine.js
+++ nodejs/readLine.js
@@ -5,44 +5,25 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-let name;
-let phoneNumber;
-let email;
-
-const question1 = () => {
+const questions = (theQuestion, result) => {
   return new Promise((resolve, reject) => {
-    rl.question('What is your name?: ', (nameInput) => {
-      name = nameInput;
-      resolve();
-    });
-  });
-};
-
-const question2 = () => {
-  return new Promise((resolve, reject) => {
-    rl.question('What is your phone number? : ', (phoneNumberInput) => {
-      phoneNumber = phoneNumberInput;
-      resolve();
-    });
-  });
-};
-const question3 = () => {
-  return new Promise((resolve, reject) => {
-    rl.question('What is your email? : ', (emailInput) => {
-      email = emailInput;
-      resolve();
+    rl.question(theQuestion, (input) => {
+      resolve(result + ' ' + input);
     });
   });
 };
 
 const main = async () => {
-  await question1();
-  await question2();
-  await question3();
-  //Here I'm calling the Function
-  console.log('Your name : ' + name);
-  console.log('Your number : ' + phoneNumber);
-  console.log('Your email : ' + email);
+  const name = await questions('What is your name? ', 'Your name : ');
+  const phoneNumber = await questions(
+    'What is your phone number? ',
+    'Your number : '
+  );
+  const email = await questions('What is your email? ', 'Your email : ');
+
+  console.log(name);
+  console.log(phoneNumber);
+  console.log(email);
   rl.close();
 };
 
