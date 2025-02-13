6970c5db4c0fe5bb08fce3e551ec513001ce8c40
dena <devdenn7@gmail.com>
2025-02-13 11:13:54 +0700

    feat: html engine with ejs

--- express.js
+++ express.js
@@ -1,21 +1,45 @@
 const express = require('express');
-const { renderHtml } = require('./utils/renderHtml');
 
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
 app.get('/', (req, res) => {
-  renderHtml('views/index.html', res);
+  res.render('index', {
+    title: 'Home page',
+    nama: 'Dena',
+  });
 });
 
 app.get('/contact', (req, res) => {
-  renderHtml('views/contact.html', res);
+  const cont = [
+    {
+      name: 'Dena',
+      email: 'Dena@test.com',
+    },
+    {
+      name: 'Dena2',
+      email: 'Dena2@test.com',
+    },
+    {
+      name: 'Dena3',
+      email: 'Dena3@test.com',
+    },
+  ];
+  res.render('contact', { title: 'Contact page', cont });
 });
 
 app.get('/about', (req, res) => {
-  renderHtml('views/about.html', res);
+  res.render('about', {
+    title: 'About page',
+  });
 });
 
+app.get('/product/:idProduct/', (req, res) => {
+  res.send(
+    `Product page with ID: ${req.params.idProduct} categoryID: ${req.query.category}`
+  );
+});
 app.use('/', (req, res) => {
   res.status(404);
   res.send('404 Page not found');
